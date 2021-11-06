5b13f7ea3c3efcd356abbb4e819a0cc6ebea9e5e
MYR <loic.themyr@gmail.com>
2021-11-06 20:14:33 +0100

    nnformer - data

--- rename.sh
+++ rename.sh
@@ -1,13 +1,10 @@
 for file in "$1/*"
 do
-	echo $file
-	echo
-    if [[ -f $file ]]; then
-        #copy stuff ....
-        new=${file/_0000/}
-        echo "$1/$file"
-        echo "$1/$new"
-        echo
-        # mv "$1/$file" "$1/$new"
-    fi
+    #copy stuff ....
+    new=${file/_0000/}
+    echo "$1/$file"
+    echo "\n"
+    echo "$1/$new"
+    echo '------\n\n'
+    # mv "$1/$file" "$1/$new"
 done
