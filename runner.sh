e7f45ccc01e439d824b985b36c577b0727d13f2b
MYR <loic.themyr@gmail.com>
2021-11-08 15:39:42 +0100

    nnformer gt - run

--- runner.sh
+++ runner.sh
@@ -10,9 +10,10 @@ source activate nnFormer
 cd ~/medtransgt
 
 # $TRAINER=nnFormerTrainerV2_Synapse
+# TRAINER="nnFormerGT0TrainerV2_Synapse"
 TRAINER="nnFormerGT1TrainerV2_Synapse"
 # TRAINER="nnFormerGT5TrainerV2_Synapse"
 
 nnFormer_train 3d_fullres $TRAINER 2 0
-# nnFormer_predict -i ../DATASET/nnFormer_raw/nnFormer_raw_data/Task002_Synapse/imagesTs -o ../DATASET/nnFormer_raw/nnFormer_raw_data/Task002_Synapse/inferTs/output -m 3d_fullres -f 0 -t 2 -chk model_best -tr $TRAINER
-# python ./nnformer/Synapse_dice_and_hd/inference.py
+nnFormer_predict -i ../DATASET/nnFormer_raw/nnFormer_raw_data/Task002_Synapse/imagesTs -o ../DATASET/nnFormer_raw/nnFormer_raw_data/Task002_Synapse/inferTs/output -m 3d_fullres -f 0 -t 2 -chk model_best -tr $TRAINER
+python ./nnformer/Synapse_dice_and_hd/inference.py
